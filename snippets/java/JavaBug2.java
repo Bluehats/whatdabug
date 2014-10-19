bee79f4f298e99b65d8ff6851ec723b83a483838
Montse Lozano Dieck <montse_967@hotmail.com>
2014-10-19 06:36:58 -0500

    java bug2 snippets

---
+++ snippets/java/JavaBug2.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Untitled {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> list;
+		
+		for(int i = 0; i < 10; i++){
+			list.add(i);
+		}
+	}
+}
