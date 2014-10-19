dbcb669ca83575260dcf496b2472f1b227b40b34
Adrian Rangel <adrian.rangel@gmail.com>
2014-10-19 06:18:16 -0500

    create first java example

---
+++ snippets/java/javaBug1.java
@@ -0,0 +1,8 @@
+class WhatDaBug {
+  public static void main(String[] args) {
+      int[] intArray = { 2, 4, 9, 1, 15 };
+      for (int i=0; i<=5; i++) {
+          System.out.println(intArray[i]);
+      }
+  }
+}
